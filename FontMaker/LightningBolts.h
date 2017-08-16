5524a4eecca79d5d494c006915dea4c70039e085
Matt Jenkins <matt@majenko.co.uk>
2017-08-16 22:45:11 +0100

    Cleaned up extra files

--- FontMaker/LightningBolts.h
+++
@@ -1,9 +0,0 @@
-#ifndef _LIGHTNINGBOLTS_H
-#define _LIGHTNINGBOLTS_H
-
-#include <DisplayCore.h>
-
-namespace Fonts {
-    extern const uint8_t LightningBolts20[];
-};
-#endif
--- FontMaker/Sentry/LCARS.h
+++
@@ -1,9 +0,0 @@
-#ifndef _LCARS_H
-#define _LCARS_H
-
-#include <DisplayCore.h>
-
-namespace Fonts {
-    extern const uint8_t LCARS32[];
-};
-#endif
